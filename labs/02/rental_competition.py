aea33483d77c90febf319b2bb60b264e9d0f9a4b
Jakub Xaver Gubas <Jakub.Xaver.Gubas@cz.ey.com>
2021-11-01 11:28:21 +0100

    commit

--- labs/02/rental_competition.py
+++ labs/02/rental_competition.py
@@ -4,9 +4,14 @@ import lzma
 import os
 import pickle
 import urllib.request
+import sklearn.compose
+import sklearn.linear_model
+import sklearn.model_selection
+import sklearn.pipeline
+import sklearn.preprocessing
 
 import numpy as np
-
+# 0332e602-165f-11e8-9de3-00505601122b
 class Dataset:
     """Rental Dataset.
 
@@ -41,12 +46,11 @@ class Dataset:
 
 parser = argparse.ArgumentParser()
 # These arguments will be set appropriately by ReCodEx, even if you change them.
-parser.add_argument("--predict", default=None, type=str, help="Run prediction on given data")
+parser.add_argument("--predict", default="rental_competition.train.npz", type=str, help="Run prediction on given data")
 parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
 # For these and any other arguments you add, ReCodEx will keep your default value.
 parser.add_argument("--model_path", default="rental_competition.model", type=str, help="Model path")
-
 def main(args: argparse.Namespace):
     if args.predict is None:
         # We are training a model.
@@ -54,8 +58,20 @@ def main(args: argparse.Namespace):
         train = Dataset()
 
         # TODO: Train a model on the given dataset and store it in `model`.
-        model = None
 
+        int_columns = np.all(train.data.astype(int) == train.data, axis=0)
+        model = sklearn.pipeline.Pipeline([
+            ("preprocess", sklearn.compose.ColumnTransformer([
+                ("onehot", sklearn.preprocessing.OneHotEncoder(handle_unknown="ignore"), int_columns),
+                ("scaler", sklearn.preprocessing.StandardScaler(), ~int_columns),
+            ])), 
+     #       ("huber", sklearn.linear_model.HuberRegressor(max_iter=10000))
+                   ("poisson", sklearn.linear_model.PoissonRegressor(max_iter=1000))
+        ])
+
+        model.fit(train.data, train.target)
+
+        
         # Serialize the model.
         with lzma.open(args.model_path, "wb") as model_file:
             pickle.dump(model, model_file)
@@ -68,7 +84,7 @@ def main(args: argparse.Namespace):
             model = pickle.load(model_file)
 
         # TODO: Generate `predictions` with the test set predictions.
-        predictions = None
+        predictions = model.predict(test.data)
 
         return predictions
 
